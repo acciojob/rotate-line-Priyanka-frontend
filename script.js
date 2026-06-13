9ce4bcd74bee61ebf01e0ad831a610fa032741ee
Priyanka-frontend <kaushikpriyanka005@gmail.com>
2026-06-13 16:41:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+
+let angle = 0;
+
+// Apply required styles
+line.style.width = '200px';
+line.style.height = '2px';
+line.style.backgroundColor = '#000000';
+line.style.position = 'absolute';
+line.style.top = '50%';
+line.style.left = '50%';
+line.style.transformOrigin = 'center center';
+
+// Rotate continuously
+setInterval(() => {
+  angle += 2;
+  line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+}, 20);
